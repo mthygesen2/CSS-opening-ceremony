2dee0db51e08a6d3e60256024fafb827d382da66
lorenzo ferrario and Mao Thygesen <maothygesen@gmail.com>
2016-03-03 09:40:11 -0800

    create intitial files and folders

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp'),
+postcss = require('gulp-postcss'),
+sass = require('gulp-sass'),
+sourcemaps = require('gulp-sourcemaps'),
+autoprefixer = require('autoprefixer');
+
+gulp.task('styles', function () {
+  var processors = [ autoprefixer
+  ];
+  return gulp.src('src/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.init())
+    .pipe(postcss(processors))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dest'));
+});
+
+gulp.task("watch", function(){
+	console.log("Watching scss files for modifications");
+	gulp.watch('src/**/*.scss', ["styles"]);
+});
+
+gulp.task('default', ['styles', 'watch']);
